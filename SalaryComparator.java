4c20abe78e025d0fba0d0d201aad13a02342ee91
Maksim-Stefanishin <135122436+Maksim-Stefanishin@users.noreply.github.com>
2023-10-26 18:33:34 +0300

    Add files via upload

---
+++ SalaryComparator.java
@@ -0,0 +1,10 @@
+package ru.gb.lesson3.hw;
+
+import java.util.Comparator;
+
+public class SalaryComparator implements Comparator<Employee> {
+    @Override
+    public int compare(Employee o1, Employee o2) {
+        return Double.compare(o2.getSalary(), o1.getSalary());
+    }
+}
